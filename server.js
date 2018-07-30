d6921f286cc513e9d017712d1af59e842bffc388
Tan Nguyen <tannguyen@Tans-MacBook-Pro.local>
2018-07-30 12:43:18 +0700

    Fix port server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const next = require('next')
 
-const port = parseInt(process.env.PORT, 10) || 3000
+const port = parseInt(process.env.PORT, 10) || 5000
 const routes = require('./routes.js')
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
+
+// Old config
+// const serve = require('serve');
+
+// const server = serve(__dirname + '/build', {
+//  port: 5000,
+//  ignore: ['node_modules']
+// })
 
 app.prepare().then(() => {
     express().use(handler).listen(port)
