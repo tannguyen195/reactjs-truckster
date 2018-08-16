a7b2e2688797c8b7fb3fa19d791e3ce89496f064
Tan Nguyen <tannguyen@Tans-MacBook-Pro.local>
2018-08-16 17:08:21 +0700

    Fix meta data in /info

--- src/components/information/InformationContainer.js
+++ src/components/information/InformationContainer.js
@@ -26,13 +26,20 @@ export default class InformationContainer extends Component {
 
     render() {
         const { infoPage, renderPage } = this.props
-
         return (
             <div >
                 {
                     infoPage && renderPage &&
                     <div>
-                        <Head />
+                        <Head
+                            url="https://gotruckster.com/"
+                            title={renderPage.title.rendered}
+                            description={renderPage.title.rendered}
+                            ogImage={renderPage.content.rendered.slice(renderPage.content.rendered.indexOf("https:"), renderPage.content.rendered.indexOf(".jpg") + 4)}
+
+                        >
+                            <meta property="og:locale" content="en_US" />
+                            <meta property="og:type" content="article" /></Head>
                         <Information parsedInfoPage={CircularJSON.parse(infoPage)} renderPage={renderPage} />
                     </div>
                 }
