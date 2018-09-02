cbba32286bd5c6d07a6c396fec32fb6a3eda22e1
Tan Nguyen <tannguyen@Tans-MacBook-Pro.local>
2018-09-02 20:55:16 +0700

    add site map

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = parseInt(process.env.PORT, 10) || 5000
 const routes = require('./routes.js')
 const app = next({ dev: process.env.NODE_ENV !== 'production' })
 const handler = routes.getRequestHandler(app)
-
+const path = require('path');
 // Old config
 // const serve = require('serve');
 
@@ -16,6 +16,21 @@ const handler = routes.getRequestHandler(app)
 
 app.prepare().then(() => {
   const server = express();
+
+
+  const options = {
+    root: path.join(__dirname, '/static'),
+    headers: {
+      'Content-Type': 'text/plain;charset=UTF-8',
+    }
+  };
+  server.get('/sitemap.xml', (req, res) => {
+    console.log("res", res)
+    return (
+      res.status(200).sendFile('sitemap.xml', options)
+    )
+  });
+
   server.use(cookieParser());
   server.use(handler).listen(port)
 })
